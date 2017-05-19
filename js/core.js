87bfd841b38d07a60e00ff9dde6d21e4dd8a0dce
Ermek Kasymbekov <fabolopfb@gmail.com>
2017-05-19 18:13:21 +0600

    first commit

---
+++ js/core.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+    $('.slider-1').slick({
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll:1
+    });
+
+});
