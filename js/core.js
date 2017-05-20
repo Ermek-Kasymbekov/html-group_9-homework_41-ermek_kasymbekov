c05ac88f4d9af6f8ea63a673975a0edb7ae7be73
Ermek Kasymbekov <fabolopfb@gmail.com>
2017-05-21 00:08:34 +0600

    without pixel perfect

--- js/core.js
+++ js/core.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 
-
-    $('.slider-1').slick({
-        dots: true,
-        adaptiveHeight: true,
-        slidesToShow: 1,
-        slidesToScroll:1
+    $(document).ready(function(){
+        $('.bxslider').bxSlider();
     });
 
 });
